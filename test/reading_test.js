059ea8120e687a0407f0eb2536b38149c57c26f6
Art Longbottom <art@tipsyelves.com>
2017-03-31 18:03:04 -0700

    added more tests

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,12 +2,16 @@ const should = require('chai').should()
 const User = require('../src/user')
 
 describe('Reading records', () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach((done) => {
     joe = new User({ name: 'Joe' })
-    joe.save()
-      .then(() => { done() })
+    maria = new User({ name: 'Maria' })
+    alex = new User({ name: 'Alex' })
+    zach = new User({ name: 'Zach' })
+
+    Promise.all([joe.save(), maria.save(), alex.save(), zach.save()])
+      .then(() => done())
   })
 
   it('finds all users by the name of `joe`', (done) => {
@@ -22,6 +26,19 @@ describe('Reading records', () => {
     User.findById(joe._id)
       .then((user) => {
         user.name.should.equal('Joe')
+        done()
+      })
+  })
+
+  it('should sort, skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 }) // -1 sort descending
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        users.length.should.equal(2)
+        users[0].name.should.equal('Joe')
+        users[1].name.should.equal('Maria')
         done()
       })
   })
